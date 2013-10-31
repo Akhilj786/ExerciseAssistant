17a1c97404dc764b07faa0ce91c60332f7a5ec9c
BharathAkhil <jain_akhil786@yahoo.com>
2013-10-30 21:30:57 -0400

    Initial Prototype
    This is demoed version.

---
+++ InitialPrototype/wpfapplication1/videopage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApplication1
+{
+    /// <summary>
+    /// Interaction logic for videopage.xaml
+    /// </summary>
+    public partial class videopage : Page
+    {
+        public videopage()
+        {
+            InitializeComponent();
+        }
+
+        private void MediaTimeline_Completed(object sender, EventArgs e)
+        {
+            this.rev_exercise_avi.Visibility = Visibility.Collapsed;
+            _mainFrame.Source = new Uri("MainMenu.xaml", UriKind.Relative);
+        }
+    }
+}
